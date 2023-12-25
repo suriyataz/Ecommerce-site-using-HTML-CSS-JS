f1be7771a62fbfcbd3475f2e9879f9e9ca899e0f
Suriya <154032582+suriyataz@users.noreply.github.com>
2023-12-25 11:31:00 +0530

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,68 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    <div class="footer-content">
+        <img src="img/dark-logo.png" class="logo" alt="">
+        <div class="footer-ul-container">
+            <ul class="category">
+                <li class="category-title">men</li>
+                <li><a href="#" class="footer-link">t-shirts</a></li>
+                <li><a href="#" class="footer-link">sweatshirts</a></li>
+                <li><a href="#" class="footer-link">shirts</a></li>
+                <li><a href="#" class="footer-link">jeans</a></li>
+                <li><a href="#" class="footer-link">trousers</a></li>
+                <li><a href="#" class="footer-link">shoes</a></li>
+                <li><a href="#" class="footer-link">casuals</a></li>
+                <li><a href="#" class="footer-link">formals</a></li>
+                <li><a href="#" class="footer-link">sports</a></li>
+                <li><a href="#" class="footer-link">watch</a></li>
+            </ul>
+            <ul class="category">
+                <li class="category-title">women</li>
+                <li><a href="#" class="footer-link">t-shirts</a></li>
+                <li><a href="#" class="footer-link">sweatshirts</a></li>
+                <li><a href="#" class="footer-link">shirts</a></li>
+                <li><a href="#" class="footer-link">jeans</a></li>
+                <li><a href="#" class="footer-link">trousers</a></li>
+                <li><a href="#" class="footer-link">shoes</a></li>
+                <li><a href="#" class="footer-link">casuals</a></li>
+                <li><a href="#" class="footer-link">formals</a></li>
+                <li><a href="#" class="footer-link">sports</a></li>
+                <li><a href="#" class="footer-link">watch</a></li>
+            </ul>
+        </div>
+    </div>
+    <p class="footer-title">About company</p>
+    <p class="info">Fashion Splash Company came into existence in the year 1992.
+
+    Essentially targeted at the domestic retails segment, we moved to Corporate Workwear and Uniforms in the
+    year 1995
+
+    We are primarily manufacturers of all categories of casual and formal apparel.
+
+    The best of inputs are used at our state of the art manufacturing facility at Sector 34 in Gurgaon to
+    give you great quality garments and accessories.
+
+    Our products come with the highest quality standards, which is the most important aspect all our
+    customers expect from us.
+
+    We are proud of our four-point quality check.</p>
+    <p class="info">Support emails - help@clothing.com, customersupport@clothing.com</p>
+    <p class="info">Telephone - 1800-000-001, 1800-000-002</p>
+    <div class="footer-social-container">
+        <div>
+            <a href="#" class="social-link">terms & services</a>
+            <a href="#" class="social-link">privacy page</a>
+        </div>
+        <div>
+            <a href="#" class="social-link">instagram</a>
+            <a href="#" class="social-link">facebook</a>
+            <a href="#" class="social-link">twitter</a>
+        </div>
+    </div>
+    <p class="footer-credit">Clothing, Best apparels online store</p>
+    `;
+}
+
+createFooter();
